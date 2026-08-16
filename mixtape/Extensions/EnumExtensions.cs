e92dc042b12a0a7904a59a91de7125a1eee6250b
Tobias Klika <tobi@swcs.pro>
2026-08-16 13:02:42 +0200

    get flags for enum

---
+++ mixtape/Extensions/EnumExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Mixtape.Extensions;
+
+public static class EnumExtensions
+{
+  /// <summary>
+  /// Get unique flags from an enum
+  /// </summary>
+  public static IEnumerable<T> GetFlags<T>(this T flags) where T : struct, Enum
+  {
+    return Enum.GetValues<T>().Where(member => flags.HasFlag(member)).ToArray();
+  }
+}
