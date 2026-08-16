d65eaf39a0fd02692db73996d11cff34855722f0
Tobias Klika <tobi@swcs.pro>
2026-08-16 13:02:23 +0200

    fix for setting current culture

--- mixtape/Context/MixtapeContextMiddleware.cs
+++ mixtape/Context/MixtapeContextMiddleware.cs
@@ -4,9 +4,14 @@ namespace Mixtape.Context
 {
   public class MixtapeContextMiddleware(RequestDelegate next)
   {
-    public async Task Invoke(HttpContext httpContext, IMixtapeContext mixtapeContext)
+    public async Task Invoke(HttpContext httpContext, IMixtapeContext mixtapeContext, ICultureResolver cultureResolver)
     {
+      // resolve mixtape context
       await mixtapeContext.Resolve(httpContext);
+      
+      // reset current culture on execution thread
+      cultureResolver.Set(cultureResolver.Current);
+      
       await next(httpContext);
     }
   }
