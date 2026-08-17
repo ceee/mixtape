f3a3be4c624c49a480d1dc2b9b4140f4112eabf6
Tobias Klika <tobi@swcs.pro>
2026-08-17 13:21:17 +0200

    new overloads for localizer

--- mixtape/Localization/LocalizerExtensions.cs
+++ mixtape/Localization/LocalizerExtensions.cs
@@ -4,32 +4,82 @@ namespace Mixtape.Localization;
 
 public static class LocalizerExtensions
 {
-  public static IHtmlContent Html(this ILocalizer localizer, string key)
+  extension(ILocalizer localizer)
   {
-    string value = localizer.Text(key);
+    /// <summary>
+    /// Get HTML from a key in the dictionary
+    /// </summary>
+    public IHtmlContent Html(string key)
+    {
+      string value = localizer.Text(key);
 
-    HtmlContentBuilder builder = new();
-    builder.SetHtmlContent(value);
-    return builder;
-  }
+      HtmlContentBuilder builder = new();
+      builder.SetHtmlContent(value);
+      return builder;
+    }
 
+    /// <summary>
+    /// Get HTML from a key in the dictionary (with token replacement)
+    /// </summary>
+    public IHtmlContent Html(string key, Dictionary<string, string> tokens)
+    {
+      string value = localizer.Text(key, tokens);
 
-  public static IHtmlContent Html(this ILocalizer localizer, string key, Dictionary<string, string> tokens)
-  {
-    string value = localizer.Text(key, tokens);
+      HtmlContentBuilder builder = new();
+      builder.SetHtmlContent(value);
+      return builder;
+    }
 
-    HtmlContentBuilder builder = new();
-    builder.SetHtmlContent(value);
-    return builder;
-  }
+    /// <summary>
+    /// Get HTML (escaped entities) from a key in the dictionary (with token replacement)
+    /// </summary>
+    public IHtmlContent HtmlEntities(string key, Dictionary<string, string> tokens = null)
+    {
+      string value = localizer.Text(key, tokens);
 
+      HtmlContentBuilder builder = new();
+      builder.SetHtmlContent(value.ToHtmlEntities());
+      return builder;
+    }
+    
+    /// <summary>
+    /// Get a text string from a key in the dictionary (with token replacement)
+    /// </summary>
+    public string Text(string key, params (string key, string value)[] tokens)
+    {
+      return localizer.Text(key, tokens.ToDictionary(x => x.key, x => x.value.ToString()));
+    }
 
-  public static IHtmlContent HtmlEntities(this ILocalizer localizer, string key, Dictionary<string, string> tokens = null)
-  {
-    string value = localizer.Text(key, tokens);
+    /// <summary>
+    /// Get a text string from a [Localize] attribute (with token replacement)
+    /// </summary>
+    public string Text<T>(T enumValue, params (string key, string value)[] tokens) where T : Enum
+    {
+      return localizer.Text(enumValue, tokens.ToDictionary(x => x.key, x => x.value.ToString()));
+    }
+    
+    /// <summary>
+    /// Only tries to resolve the key when it is prefixed with an @ (with token replacement)
+    /// </summary>
+    public string Maybe(string key, params (string key, string value)[] tokens)
+    {
+      return localizer.Maybe(key, tokens.ToDictionary(x => x.key, x => x.value.ToString()));
+    }
+    
+    /// <summary>
+    /// Get HTML from a key in the dictionary (with token replacement)
+    /// </summary>
+    public IHtmlContent Html(string key, params (string key, string value)[] tokens)
+    {
+      return localizer.Html(key, tokens.ToDictionary(x => x.key, x => x.value.ToString()));
+    }
 
-    HtmlContentBuilder builder = new();
-    builder.SetHtmlContent(value.ToHtmlEntities());
-    return builder;
+    /// <summary>
+    /// Get HTML (escaped entities) from a key in the dictionary (with token replacement)
+    /// </summary>
+    public IHtmlContent HtmlEntities(string key, params (string key, string value)[] tokens)
+    {
+      return localizer.HtmlEntities(key, tokens.ToDictionary(x => x.key, x => x.value.ToString()));
+    }
   }
 }
--- mixtape/Utils/TokenReplacement.cs
+++ mixtape/Utils/TokenReplacement.cs
@@ -4,50 +4,20 @@ namespace Mixtape.Utils;
 
 public class TokenReplacement
 {
-  static Regex TokenRegex;
+  static readonly Regex TokenRegex = new(@"{([\w.-]+)}", RegexOptions.Compiled | RegexOptions.IgnoreCase); // "{([\\w-_.]+)}"
 
 
-  static TokenReplacement()
+  public static string Apply(string text, IReadOnlyDictionary<string, string> tokens)
   {
-    TokenRegex = new Regex("{([\\w-_.]+)}", RegexOptions.IgnoreCase);
-  }
-
-
-  public static string Apply(string text, Dictionary<string, string> tokens)
-  {
-    if (text.IsNullOrWhiteSpace())
+    if (text.IsNullOrWhiteSpace() || tokens.Count == 0)
     {
       return text;
     }
-
-    MatchCollection matches = TokenRegex.Matches(text);
-
-    foreach (Match match in matches)
+    
+    return TokenRegex.Replace(text, match =>
     {
-      if (!match.Success)
-      {
-        continue;
-      }
-
-      string original = match.Value;
       string token = match.Groups[1].Value;
-
-      if (tokens.ContainsKey(token))
-      {
-        text = text.Replace(original, tokens[token]);
-      }
-    }
-
-    return text;
-
-    //foreach ((string key, string value) in tokens)
-    //{
-    //  string tokenKey = key.EnsureStartsWith(BEGIN).EnsureEndsWith(END);
-    //  string tokenValue = value; // TODO escape for HTML?
-
-    //  text = text.Replace(tokenKey, value);
-    //}
-
-    //return text;
+      return tokens.TryGetValue(token, out string value) ? value : string.Empty;
+    });
   }
 }
