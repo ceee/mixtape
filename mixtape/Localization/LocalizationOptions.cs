15c6c1ed03594375d8c35a4c1ca9a2114f9f7a28
Tobias Klika <tobi@swcs.pro>
2026-09-08 15:47:52 +0200

    fix when language keys are empty

--- mixtape/Localization/LocalizationOptions.cs
+++ mixtape/Localization/LocalizationOptions.cs
@@ -5,8 +5,8 @@ public class LocalizationOptions
   public string FilePath { get; set; }
   
   public bool CaseInsensitiveKeys { get; set; } = false;
-  
-  public LocalizationLanguageKeys Keys { get; set; }
+
+  public LocalizationLanguageKeys Keys { get; set; } = [];
 }
 
 public class LocalizationLanguageKeys : Dictionary<string, Dictionary<string, string>>
